f4d88aad711ccc5fba210dd906e130206a8e2cdf
Francisco Nardi <116312660+Cisconardi@users.noreply.github.com>
2025-06-20 00:45:05 +0200

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def run_crawl(url: str, export_tabs: list, export_format: str, output_folder: str):
+    export_tabs_str = ",".join(export_tabs)
+    command = [
+        "/opt/screamingfrogseospider/screamingfrogseospider",
+        "--headless",
+        "--crawl", url,
+        "--save-crawl",
+        "--output-folder", output_folder,
+        "--export-tabs", export_tabs_str,
+        "--export-format", export_format
+    ]
+    subprocess.run(command, check=True)
